754ea2a98ec29845496972c2b68371fba36aa519
Andrew Barba <andrew@tablelist.com>
2014-08-21 14:54:24 -0400

    check for leading slash

--- src/_lib/http.js
+++ src/_lib/http.js
@@ -54,6 +54,11 @@ angular
         HTTP.prototype.apiUrl = function(endpoint, params) {
             params = params || {};
             
+            // use leading slash
+            if (endpoint.slice(0,1) != '/') {
+                endpoint = '/' + endpoint;
+            }
+
             // create url parameter string
             var data = [];
             var keys = Object.keys(params);
