987303d08b7252f5abff3430215bc9983784bd8b
Andrew Barba <andrew@tablelist.com>
2014-08-24 22:09:02 -0400

    fix query errors

--- src/_lib/query.js
+++ src/_lib/query.js
@@ -1,21 +1,12 @@
 
 angular
 	.module('tl')
-	.factory('tl.query', ['tl.config', function(config){
+	.factory('tl.query', ['tl.config','$location', function(config, $location){
 
 		var Query = function(){};
 
 		Query.prototype.params = function() {
-			var params = {};
-			var parts = window.location.search.replace('?','').split('&');
-			for (var i = 0; i < parts.length; i++) {
-				var part = parts[i];
-				var kv = part.split('=');
-				if (kv && kv.length == 2) {
-				    params[kv[0]] = decodeURIComponent(kv[1]);
-				}
-			}
-			return params;
+			return $location.search();
 		};
 
 		Query.prototype.get = function(key) {
@@ -23,22 +14,9 @@ angular
 		};
 
 		Query.prototype.set = function(key, value) {
-			var params = this.params();
-			params[key] = value;
-			var keys = Object.keys(params);
-			var parts = [];
-			for (var i = 0; i < keys.length; i++) {
-				var key = keys[i];
-				var val = params[key];
-				var string = encodeURIComponent(key) + '=' + encodeURIComponent(val);
-				parts.push(string);
-			}
-			var search = '?' + parts.join('&');
-			if (window.history && window.history.pushState) {
-				window.history.pushState({}, '', search);
-			} else {
-				window.location.search = search;
-			}
+			var data = {};
+			data[key] = value;
+			$location.search(data);
 		};
 
 		return new Query();
