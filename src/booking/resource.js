8856e9170d1b6a590bd2ecc19c101eabbeb54f4c
Tim Officer <timothyofficer@gmail.com>
2014-10-09 16:11:10 -0400

    bump version, add booking create

--- src/booking/resource.js
+++ src/booking/resource.js
@@ -1,28 +1,31 @@
-
 angular
-	.module('tl')
-	.factory('tl.booking.resource', ['tl.resource', function(resource){
-		
-		var endpoint = '/booking/:id';
+  .module('tl')
+  .factory('tl.booking.resource', ['tl.resource', function(resource) {
 
-		return resource(endpoint, {
-			id: '@id'
-		}, {
-			void: {
-				method: 'POST',
-				url: endpoint + '/void'
-			},
-			refund: {
-				method: 'POST',
-				url: endpoint + '/refund'
-			},
-			join: {
-				method: 'POST',
-				url: 'booking/join'
-			},
-			accept: {
-				method: 'POST',
-				url: endpoint + '/accept'
-			}
-		});
-	}]);
+    var endpoint = '/booking/:id';
+
+    return resource(endpoint, {
+      id: '@id'
+    }, {
+      create: {
+        method: 'POST',
+        url: 'booking',
+      },
+      void: {
+        method: 'POST',
+        url: endpoint + '/void'
+      },
+      refund: {
+        method: 'POST',
+        url: endpoint + '/refund'
+      },
+      join: {
+        method: 'POST',
+        url: 'booking/join'
+      },
+      accept: {
+        method: 'POST',
+        url: endpoint + '/accept'
+      }
+    });
+  }]);
--- src/booking/service.js
+++ src/booking/service.js
@@ -1,36 +1,39 @@
-
 angular
-	.module('tl')
-	.service('tl.booking.service', ['tl.booking.resource', 'tl.service', function(Booking, Service){
+  .module('tl')
+  .service('tl.booking.service', ['tl.booking.resource', 'tl.service', function(Booking, Service) {
 
-		var BookingService = Service.extend(Booking);
+    var BookingService = Service.extend(Booking);
 
-		BookingService.prototype.void = function(id, notify, success, error) {
-			return Booking.void({}, {
-				id: id,
-				notify: notify ? true : false
-			}, success, error);
-		};
+    BookingService.prototype.create = function(data, success, error) {
+      return Booking.create({}, data, success, error);
+    };
 
-		BookingService.prototype.refund = function(id, amount, reason, success, error) {
-			return Booking.refund({}, {
-				id: id,
-				money: amount,
-				reason: reason
-			}, success, error);
-		};
+    BookingService.prototype.void = function(id, notify, success, error) {
+      return Booking.void({}, {
+        id: id,
+        notify: notify ? true : false
+      }, success, error);
+    };
 
-		BookingService.prototype.join = function(splitCode, success, error) {
-			return Booking.join({}, {
-				splitCode: splitCode,
-			}, success, error);
-		};
+    BookingService.prototype.refund = function(id, amount, reason, success, error) {
+      return Booking.refund({}, {
+        id: id,
+        money: amount,
+        reason: reason
+      }, success, error);
+    };
 
-		BookingService.prototype.accept = function(id, success, error) {
-			return Booking.accept({}, {
-				id: id
-			}, success, error);
-		};
+    BookingService.prototype.join = function(splitCode, success, error) {
+      return Booking.join({}, {
+        splitCode: splitCode,
+      }, success, error);
+    };
 
-		return new BookingService();
-	}]);
+    BookingService.prototype.accept = function(id, success, error) {
+      return Booking.accept({}, {
+        id: id
+      }, success, error);
+    };
+
+    return new BookingService();
+  }]);
