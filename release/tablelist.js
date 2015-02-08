1f30aa11bd9041e550539f28e5a2efe0283a55e8
Tim Officer <timothyofficer@gmail.com>
2015-02-08 17:19:10 -0500

    release

--- release/tablelist.js
+++ release/tablelist.js
@@ -180,151 +180,155 @@ angular
 
 angular
   .module('tl')
-  .factory('tl.service', ['tl.http', function(http) {
-    'use strict';
+  .factory('tl.service', [
+    'tl.http',
+    'tl.keychain',
+    function(http, tlKeychain) {
+      'use strict';
 
-    var Service = function(resource) {
-      this.resource = resource;
-    };
-
-    var extend = function(resource) {
-      var ExtendedService = function() {
-        Service.call(this, resource);
+      var Service = function(resource) {
+        this.resource = resource;
       };
 
-      var proto = Service.prototype;
-      var keys = Object.keys(proto);
-      for (var i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        if (key != 'constructor') {
-          ExtendedService.prototype[key] = proto[key];
+      var extend = function(resource) {
+        var ExtendedService = function() {
+          Service.call(this, resource);
+        };
+
+        var proto = Service.prototype;
+        var keys = Object.keys(proto);
+        for (var i = 0; i < keys.length; i++) {
+          var key = keys[i];
+          if (key != 'constructor') {
+            ExtendedService.prototype[key] = proto[key];
+          }
         }
+
+        return ExtendedService;
+      };
+
+      /*==============================================================*
+      /* Constants
+      /*==============================================================*/
+
+      var DEFAULT_LIMIT = 100;
+      var DEFAULT_SORT = '-created';
+
+      /*==============================================================*
+      /* CRUD
+      /*==============================================================*/
+
+      Service.prototype.create = function(data, success, error) {
+        return this.resource.save({}, data, success, error);
+      };
+
+      Service.prototype.read = function(id, success, error) {
+        return this.resource.get({
+          id: id
+        }, success, error);
+      };
+
+      Service.prototype.update = function(id, data, success, error) {
+        return this.resource.update({
+          id: id
+        }, data, success, error);
+      };
+
+      Service.prototype.delete = function(id, success, error) {
+        return this.resource.delete({
+          id: id
+        }, success, error);
+      };
+
+      Service.prototype.list = function(limit, sort, success, error) {
+        return this.resource.query({
+          sort: sort || DEFAULT_SORT,
+          limit: limit || DEFAULT_LIMIT
+        }, success, error);
+      };
+
+      /*==============================================================*
+      /* Query
+      /*==============================================================*/
+
+      Service.prototype.query = function(query, limit, sort, success, error) {
+        var queryString = this.buildQueryString(query);
+        if (!queryString) return null;
+
+        return this.resource.query({
+          query: queryString,
+          sort: sort || DEFAULT_SORT,
+          limit: limit || DEFAULT_LIMIT
+        }, success, error);
+      };
+
+      Service.prototype.totals = function(query, sort, success, error) {
+        var queryString = this.buildQueryString(query);
+        if (!queryString) return null;
+
+        return this.resource.query({
+          total: true,
+          query: queryString,
+          sort: sort || DEFAULT_SORT
+        }, success, error);
+      };
+
+      Service.prototype.buildQueryString = function(query, next) {
+        try {
+          return JSON.stringify(query);
+        } catch (err) {
+          return null;
+        }
+      };
+
+      /*==============================================================*
+      /* Images
+      /*==============================================================*/
+
+      Service.prototype.listImages = function(id, success, error) {
+        return this.resource.listImages({
+          id: id
+        }, success, error);
+      };
+
+      Service.prototype.addImage = function(id, image, success, error) {
+        image.id = id;
+        return this.resource.addImage(image, success, error);
+      };
+
+      Service.prototype.deleteImage = function(id, imageId, success, error) {
+        return this.resource.deleteImage({
+          id: id,
+          imageId: imageId
+        }, success, error);
+      };
+
+      Service.prototype.setPrimaryImage = function(id, imageId, success, error) {
+        return this.resource.setPrimaryImage({
+          id: id,
+          imageId: imageId
+        }, success, error);
+      };
+
+      Service.prototype.exportUrl = function(query, sort, format) {
+        var endpoint = this.resource.ENDPOINT;
+        var index = this.resource.ENDPOINT.indexOf(":");
+        if (index > -1) endpoint = endpoint.substring(0, index);
+        var url = this.resource.URL.substring(0, this.resource.URL.lastIndexOf(":") - 1);
+        url += "?query=" + encodeURIComponent(JSON.stringify(query));
+        url += "&sort=" + sort;
+        url += "&admin=" + true;
+        url += "&csv=" + true;
+        url += "&auth=" + tlKeychain.authToken();
+        return url;
+      };
+
+      return {
+        extend: extend,
+        common: Service
       }
-
-      return ExtendedService;
-    };
-
-    /*==============================================================*
-    /* Constants
-    /*==============================================================*/
-
-    var DEFAULT_LIMIT = 100;
-    var DEFAULT_SORT = '-created';
-
-    /*==============================================================*
-    /* CRUD
-    /*==============================================================*/
-
-    Service.prototype.create = function(data, success, error) {
-      return this.resource.save({}, data, success, error);
-    };
-
-    Service.prototype.read = function(id, success, error) {
-      return this.resource.get({
-        id: id
-      }, success, error);
-    };
-
-    Service.prototype.update = function(id, data, success, error) {
-      return this.resource.update({
-        id: id
-      }, data, success, error);
-    };
-
-    Service.prototype.delete = function(id, success, error) {
-      return this.resource.delete({
-        id: id
-      }, success, error);
-    };
-
-    Service.prototype.list = function(limit, sort, success, error) {
-      return this.resource.query({
-        sort: sort || DEFAULT_SORT,
-        limit: limit || DEFAULT_LIMIT
-      }, success, error);
-    };
-
-    /*==============================================================*
-    /* Query
-    /*==============================================================*/
-
-    Service.prototype.query = function(query, limit, sort, success, error) {
-      var queryString = this.buildQueryString(query);
-      if (!queryString) return null;
-
-      return this.resource.query({
-        query: queryString,
-        sort: sort || DEFAULT_SORT,
-        limit: limit || DEFAULT_LIMIT
-      }, success, error);
-    };
-
-    Service.prototype.totals = function(query, sort, success, error) {
-      var queryString = this.buildQueryString(query);
-      if (!queryString) return null;
-
-      return this.resource.query({
-        total: true,
-        query: queryString,
-        sort: sort || DEFAULT_SORT
-      }, success, error);
-    };
-
-    Service.prototype.buildQueryString = function(query, next) {
-      try {
-        return JSON.stringify(query);
-      } catch (err) {
-        return null;
-      }
-    };
-
-    /*==============================================================*
-    /* Images
-    /*==============================================================*/
-
-    Service.prototype.listImages = function(id, success, error) {
-      return this.resource.listImages({
-        id: id
-      }, success, error);
-    };
-
-    Service.prototype.addImage = function(id, image, success, error) {
-      image.id = id;
-      return this.resource.addImage(image, success, error);
-    };
-
-    Service.prototype.deleteImage = function(id, imageId, success, error) {
-      return this.resource.deleteImage({
-        id: id,
-        imageId: imageId
-      }, success, error);
-    };
-
-    Service.prototype.setPrimaryImage = function(id, imageId, success, error) {
-      return this.resource.setPrimaryImage({
-        id: id,
-        imageId: imageId
-      }, success, error);
-    };
-
-    Service.prototype.exportUrl = function(query, sort, format) {
-      var endpoint = this.resource.ENDPOINT;
-      var index = this.resource.ENDPOINT.indexOf(":");
-      if (index > -1) endpoint = endpoint.substring(0, index);
-      var url = this.resource.URL.substring(0, this.resource.URL.lastIndexOf(":") - 1);
-      url += "?query=" + encodeURIComponent(JSON.stringify(query));
-      url += "&sort=" + sort;
-      url += "&admin=" + true;
-      url += "&csv=" + true;
-      url += "&auth=" + tlKeychain.authToken();
-      return url;
-    };
-
-    return {
-      extend: extend,
-      common: Service
     }
-  }]);
+  ]);
 
 /**
  * Angular module for setting, reading and removing cookies
@@ -1248,68 +1252,6 @@ angular.module('tl').service('tl.booking.service', [
 
 angular
 	.module('tl')
-	.service('tl.campaign', ['tl.campaign.resource', 'tl.campaign.service', function(resource, service){
-		this.resource = resource;
-		this.service = service;
-	}]);
-
-angular
-	.module('tl')
-	.factory('tl.campaign.resource', ['tl.resource', function(resource){
-		
-		var endpoint = '/campaign/:id';
-
-		return resource(endpoint, {
-			id: '@id'
-		}, {
-			
-		});
-	}]);
-
-angular
-	.module('tl')
-	.service('tl.campaign.service', ['tl.storage', 'tl.campaign.resource', 'tl.service', function(storage, Campaign, Service){
-		
-		var CampaignService = Service.extend(User);
-
-		/**
-		 * List internal campaigns
-		 */
-		CampaignService.prototype.listInternal = function() {
-			return Campaign.list({ internal : true }).$promise;
-		};
-
-		return new CampaignService();
-	}]);
-
-angular
-	.module('tl')
-	.service('tl.city', ['tl.city.resource', 'tl.city.service', function(resource, service){
-		this.resource = resource;
-		this.service = service;
-	}]);
-
-angular
-	.module('tl')
-	.factory('tl.city.resource', ['tl.resource', function(resource){
-		return resource('/city/:id', {
-			id: '@id'
-		}, {
-			// no extra methods
-		});
-	}]);
-
-angular
-	.module('tl')
-	.service('tl.city.service', ['tl.service', 'tl.city.resource', function(Service, City){
-
-		var CityService = Service.extend(City);
-
-		return new CityService();
-	}]);
-
-angular
-	.module('tl')
 	.service('tl.event', ['tl.event.resource', 'tl.event.service', function(resource, service){
 		this.resource = resource;
 		this.service = service;
@@ -1394,6 +1336,68 @@ angular
     return new EventService();
   }]);
 
+
+angular
+	.module('tl')
+	.service('tl.city', ['tl.city.resource', 'tl.city.service', function(resource, service){
+		this.resource = resource;
+		this.service = service;
+	}]);
+
+angular
+	.module('tl')
+	.factory('tl.city.resource', ['tl.resource', function(resource){
+		return resource('/city/:id', {
+			id: '@id'
+		}, {
+			// no extra methods
+		});
+	}]);
+
+angular
+	.module('tl')
+	.service('tl.city.service', ['tl.service', 'tl.city.resource', function(Service, City){
+
+		var CityService = Service.extend(City);
+
+		return new CityService();
+	}]);
+
+angular
+	.module('tl')
+	.service('tl.campaign', ['tl.campaign.resource', 'tl.campaign.service', function(resource, service){
+		this.resource = resource;
+		this.service = service;
+	}]);
+
+angular
+	.module('tl')
+	.factory('tl.campaign.resource', ['tl.resource', function(resource){
+		
+		var endpoint = '/campaign/:id';
+
+		return resource(endpoint, {
+			id: '@id'
+		}, {
+			
+		});
+	}]);
+
+angular
+	.module('tl')
+	.service('tl.campaign.service', ['tl.storage', 'tl.campaign.resource', 'tl.service', function(storage, Campaign, Service){
+		
+		var CampaignService = Service.extend(User);
+
+		/**
+		 * List internal campaigns
+		 */
+		CampaignService.prototype.listInternal = function() {
+			return Campaign.list({ internal : true }).$promise;
+		};
+
+		return new CampaignService();
+	}]);
 angular
   .module('tl')
   .service('tl.image', ['tl.image.resource', 'tl.image.service',
