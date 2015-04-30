3c38e41995a820fdb855a86715f2dafda5382322
Andrew Barba <abarba.77@gmail.com>
2015-04-30 11:31:54 -0400

    refacotr payment profile functions

--- src/payment-profile/service.js
+++ src/payment-profile/service.js
@@ -6,7 +6,38 @@ angular
 
       var PaymentService = Service.extend(Payment);
 
+      PaymentService.prototype.addPaymentProfile = function(options) {
+        var data = {
+          cardholderName: options.name,
+          cardNumber: utils.digits(options.number),
+          cardExpMonth: utils.digits(options.month),
+          cardExpYear: utils.digits(options.year),
+          cardCvv: utils.digits(options.cvv),
+          cardZip: utils.digits(options.zip)
+        }
+        return this.create(data).$promise;
+      };
+
+      PaymentService.prototype.updatePaymentProfile = function(options) {
+        var profileId = options.id;
+        if (!profileId) {
+          throw "An existing profile is required. Missing options.id";
+        }
+        var data = {
+          cardholderName: options.name,
+          cardNumber: utils.digits(options.number),
+          cardExpMonth: utils.digits(options.month),
+          cardExpYear: utils.digits(options.year),
+          cardCvv: utils.digits(options.cvv),
+          cardZip: utils.digits(options.zip)
+        }
+        return this.update(profileId, data).$promise;
+      };
+
+      // Deprecated
+
       PaymentService.prototype.addProfile = function(name, number, month, year, cvv, address, city, state, zip, success, error) {
+        console.log('DEPRECATED - use .addPaymentProfile');
         var data = {
           cardholderName: name,
           cardNumber: utils.digits(number),
@@ -25,6 +56,7 @@ angular
       };
 
       PaymentService.prototype.updateProfile = function(profileId, name, number, month, year, cvv, address, city, state, zip, success, error) {
+        console.log('DEPRECATED - use .updatePaymentProfile');
         if (!profileId) {
           return this.addProfile(name, number, month, year, cvv, address, city, state, zip, success, error);
         }
