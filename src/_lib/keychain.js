cc77ff7ec746778221f9d6df8dea906026ceb98b
Andrew Barba <abarba.77@gmail.com>
2015-05-28 22:12:59 -0400

    updates for in-memory auth and prospect token to help fix issues with private browsing sessions

--- src/_lib/keychain.js
+++ src/_lib/keychain.js
@@ -4,22 +4,38 @@ angular
     function(cookie, utils, config) {
       'use strict';
 
+      // constants
+
       var SUFFIX = config.ENV_PROD ? '' : '_dev';
       var AUTH_KEY = 'tl_auth' + SUFFIX;
       var PROSPECT_KEY = 'tl_prospect' + SUFFIX;
 
+      // in-memory auth and prospect tokens
+
+      var _authToken = null;
+      var _prospectToken = null;
+
+      // service
+
       var Keychain = function() {};
 
       Keychain.prototype.authToken = function() {
-        return cookie.get(AUTH_KEY);
+        return _authToken || (function(){
+          _authToken = cookie.get(AUTH_KEY);
+          return _authToken;
+        })();
       };
 
       Keychain.prototype.setAuthToken = function(token) {
+        _authToken = token || null;
         return cookie.set(AUTH_KEY, token);
       };
 
       Keychain.prototype.prospectToken = function() {
-        var token = cookie.get(PROSPECT_KEY);
+        var token = _prospectToken || (function(){
+          _prospectToken = cookie.get(PROSPECT_KEY);
+          return _prospectToken;
+        })();
         if (!token) {
           token = utils.guid(16, '-');
           this.setProspectToken(token);
@@ -28,6 +44,7 @@ angular
       };
 
       Keychain.prototype.setProspectToken = function(token) {
+        _prospectToken = token || null;
         return cookie.set(PROSPECT_KEY, token);
       };
 
