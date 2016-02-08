bfeb67e60b7053ab31b71f7560dc4a83edd3a156
Alex Johnson <alex@tablelist.com>
2016-02-08 10:48:37 -0500

    Increment version. Add build

--- release/tablelist.js
+++ release/tablelist.js
@@ -1,5 +1,5 @@
 /**
- * Tablelist.js 1.4.0
+ * Tablelist.js
  *
  * Dependencies:
  *  - http://ajax.googleapis.com/ajax/libs/angularjs/1.2.x/angular.min.js
@@ -1970,6 +1970,33 @@ angular
     /* Cities
     /*==============================================================*/
 
+   /**
+    * List of events for the provided city. 
+    * Returns a promise. Supports additional 'options'.
+    *
+    * @method listEventsForCity
+    * @param {Object} options 
+    * @param {String} options.cityId - ID of a city
+    * @param {String} options.fields - CSV of fields to return
+    * @param {String} options.start  - unix start date for events
+    * @param {String} options.end    - unix end date for events
+    */
+
+    EventService.prototype.listEventsForCity = function(options) {
+      if (!options) throw new Error('options is required');
+      if (!options.cityId) throw new Error('options.cityId is required');
+
+      return Event.listForCity(options).$promise;
+    };
+
+   /**
+    * List of events for the provided city. 
+    * Legacy support. 
+    *
+    * @method listForCity
+    * @param {String} cityId - ID of a city
+    */
+
     EventService.prototype.listForCity = function(cityId, success, error) {
       return Event.listForCity({
         cityId: cityId
@@ -2812,6 +2839,14 @@ angular
 
       var PaymentService = Service.extend(Payment);
 
+       /**
+        * Create a new payment profile with
+        * a nonce token from braintree
+        *
+        * @method addPaymentMethodNonce
+        * @param {Object} options
+        * @param {String} options.paymentMethodNonce - nonce token from braintree
+        */
       PaymentService.prototype.addPaymentMethodNonce = function(options) {
         var data = {
           paymentMethodNonce : options.paymentMethodNonce,
@@ -2820,6 +2855,14 @@ angular
         return this.create(data).$promise;
       };
 
+      /**
+       * Set a payment profile as the user's default.
+       * Removes the 'default' flag from the previous default profile.
+       *
+       * @method setDefaultPaymentProfile
+       * @param {Object} options
+       * @param {String} options.id - payment profile id
+       */
       PaymentService.prototype.setDefaultPaymentProfile = function(options) {
 
         var profileId = options.id;
@@ -2834,6 +2877,20 @@ angular
         return this.update(profileId, data).$promise;
       };
 
+      /**
+       * Creates a new payment profile on a user
+       * from an entered credit card.
+       *
+       * @deprecated
+       * @method addPaymentProfile
+       * @param {Object} options
+       * @param {String} options.name
+       * @param {String} options.number
+       * @param {String} options.month
+       * @param {String} options.year
+       * @param {String} options.cvv
+       * @param {String} options.zip
+       */
       PaymentService.prototype.addPaymentProfile = function(options) {
         console.log('DEPRECATED - use .addPaymentMethodNonce');
 
@@ -2848,6 +2905,19 @@ angular
         return this.create(data).$promise;
       };
 
+      /**
+       * Updates a payment profile on a user
+       *
+       * @deprecated
+       * @method updatePaymentProfile
+       * @param {Object} options
+       * @param {String} options.name
+       * @param {String} options.number
+       * @param {String} options.month
+       * @param {String} options.year
+       * @param {String} options.cvv
+       * @param {String} options.zip
+       */
       PaymentService.prototype.updatePaymentProfile = function(options) {
 
         var profileId = options.id;
@@ -2866,8 +2936,25 @@ angular
         return this.update(profileId, data).$promise;
       };
 
-      // Deprecated
-
+      
+      /**
+       * Creates a new payment profile on a user
+       * from an entered credit card.
+       *
+       * @deprecated
+       * @method addPaymentProfile
+       * @param {String} name
+       * @param {String} number
+       * @param {String} month
+       * @param {String} year
+       * @param {String} cvv
+       * @param {String} address
+       * @param {String} city
+       * @param {String} state
+       * @param {String} zip
+       * @param {Function} success
+       * @param {Function} error
+       */
       PaymentService.prototype.addProfile = function(name, number, month, year, cvv, address, city, state, zip, success, error) {
         console.log('DEPRECATED - use .addPaymentProfile');
         var data = {
@@ -2887,6 +2974,24 @@ angular
         return this.create(data, success, error);
       };
 
+      /**
+       * Updates a payment profile on a user
+       *
+       * @deprecated
+       * @method addPaymentProfile
+       * @param {String} profileId
+       * @param {String} name
+       * @param {String} number
+       * @param {String} month
+       * @param {String} year
+       * @param {String} cvv
+       * @param {String} address
+       * @param {String} city
+       * @param {String} state
+       * @param {String} zip
+       * @param {Function} success
+       * @param {Function} error
+       */
       PaymentService.prototype.updateProfile = function(profileId, name, number, month, year, cvv, address, city, state, zip, success, error) {
         console.log('DEPRECATED - use .updatePaymentProfile');
         if (!profileId) {
@@ -3397,35 +3502,6 @@ angular
 
 		return new SettingsService();
 	}]);
-
-angular
-	.module('tl')
-	.service('tl.table', ['tl.table.resource', 'tl.table.service', function(resource, service){
-		this.resource = resource;
-		this.service = service;
-	}]);
-
-angular
-	.module('tl')
-	.factory('tl.table.resource', ['tl.resource', function(resource){
-
-		var endpoint = '/table/:id';
-
-		return resource(endpoint, {
-			id: '@id'
-		}, {
-			// add additional methods here
-		});
-	}]);
-
-angular
-	.module('tl')
-	.service('tl.table.service', ['tl.service', 'tl.table.resource', function(Service, Table){
-
-		var TableService = Service.extend(Table);
-
-		return new TableService();
-	}]);
 angular
   .module('tl')
   .service('tl.support', [
@@ -3478,6 +3554,35 @@ angular
     }
   ]);
 
+
+angular
+	.module('tl')
+	.service('tl.table', ['tl.table.resource', 'tl.table.service', function(resource, service){
+		this.resource = resource;
+		this.service = service;
+	}]);
+
+angular
+	.module('tl')
+	.factory('tl.table.resource', ['tl.resource', function(resource){
+
+		var endpoint = '/table/:id';
+
+		return resource(endpoint, {
+			id: '@id'
+		}, {
+			// add additional methods here
+		});
+	}]);
+
+angular
+	.module('tl')
+	.service('tl.table.service', ['tl.service', 'tl.table.resource', function(Service, Table){
+
+		var TableService = Service.extend(Table);
+
+		return new TableService();
+	}]);
 angular
   .module('tl')
   .service('tl.tag', [
@@ -3597,6 +3702,7 @@ angular
 		EventViewed: "TLEventViewed",
 
 		// Booking Flow
+		BookingInventoryViewed: "TLBookingInventoryViewed",
 		BookingAddBottlesViewed: "TLBookingAddBottlesViewed",
 		BookingInfoViewed: "TLBookingInfoViewed",
 		BookingReviewViewed: "TLBookingReviewViewed",
